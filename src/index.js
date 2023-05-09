b0fb8bf3bd50a21d4309a2f21c54628364599b3a
Joab Owala <owalajoab@gmail.com>
2023-05-09 10:49:22 +0300

    Adding events (onClick, onMouseOver)

--- src/index.js
+++ src/index.js
@@ -38,12 +38,26 @@ const books = [
 }
 
 function Book({img, title, author}){
-  // const {img, title, author} = props
+  //adding an event
+  //we need an attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert('hello Again')
+  }
+  const complexExample = () =>{
+    console.log(author)
+  }
   return (
-    <article className='book'>
+    <article 
+    className='book' 
+    onMouseOver={() => {console.log(title)}}>
     <img src={img} alt={title} />
     <h3>{title}</h3>
     <h6>{author}</h6>
+    <button type='button' onClick={clickHandler}>refrenceExample</button>
+    <button type='button' onClick={complexExample}>complexExample</button>
     </article>
   )
 }
