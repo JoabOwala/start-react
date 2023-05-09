97897586caa4501935ae6747c67b00d9ea1528c1
Joab Owala <owalajoab@gmail.com>
2023-05-09 09:13:40 +0300

    refactored with an array with objects and using map

--- src/index.js
+++ src/index.js
@@ -4,44 +4,45 @@ import ReactDOM from 'react-dom'
 //CSS
 import './index.css'
 
-// Setup vars
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/51rvfEAyQwL._SY346_.jpg',
-  title: 'The House at the End of the World',
-  author: 'Dean Koontz'
-}
+const books = [ 
+  {
+    id:1,
+    img: 'https://m.media-amazon.com/images/I/51rvfEAyQwL._SY346_.jpg',
+    title: 'The House at the End of the World',
+    author: 'Dean Koontz'
+  }, 
+  {
+    id:2,
+    img: 'https://m.media-amazon.com/images/I/51H5F5oixaL._SY346_.jpg',
+    title: 'Quantum Radio',
+    author: 'A.G. Riddle'
+  },
+  {
+    id:3,
+    img: 'https://m.media-amazon.com/images/I/412nfVVHybL._SY346_.jpg',
+    title: 'Twins (an Ell Donsaii story #17)',
+    author: 'Laurence Dahners'
+  },
+] 
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/51H5F5oixaL._SY346_.jpg',
-  title: 'Quantum Radio',
-  author: 'A.G. Riddle'
-}
-function Booklist(){
+  function Booklist(){
   return (
     <section className='booklist'>
-    <Book 
-    img={firstBook.img} 
-    title={firstBook.title} 
-    author={firstBook.author}
-    >
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, hic illum dolorem sint nobis consectetur est. Suscipit necessitatibus asperiores exercitationem!</p>
-    </Book>
-    <Book 
-    img={secondBook.img} 
-    title={secondBook.title} 
-    author={secondBook.author}
-    />
+    {books.map((book) =>{
+      return (
+        <Book key={book.id} book={book}></Book>
+      )
+    })}
     </section>
   )
 }
 
 function Book(props){
-  const {img, title, author, children} = props
+  const {img, title, author} = props.book
   return (
     <article className='book'>
     <img src={img} alt={props.title} />
     <h3>{title}</h3>
-    {children}
     <h6>{author}</h6>
     </article>
   )
